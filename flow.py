096083f43bf5693899086fbbf2272e75843beccf
xiyangxixi <34201743+xiyangxixi@users.noreply.github.com>
2018-07-10 15:11:12 +0800

    网卡流量监控

---
+++ flow.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+try:
+    import psutil
+except ImportError:
+    print('Error: psutil module not found!')
+    exit()
+
+
+def get_key():
+
+    key_info = psutil.net_io_counters(pernic=True).keys()
+
+    recv = {}
+    sent = {}
+
+    for key in key_info:
+        recv.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_recv)
+        sent.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_sent)
+
+    return key_info, recv, sent
+
+
+def get_rate(func):
+
+    import time
+
+    key_info, old_recv, old_sent = func()
+
+    time.sleep(1)
+
+    key_info, now_recv, now_sent = func()
+
+    net_in = {}
+    net_out = {}
+
+    for key in key_info:
+        net_in.setdefault(key, (now_recv.get(key) - old_recv.get(key)) / 1024)
+        net_out.setdefault(key, (now_sent.get(key) - old_sent.get(key)) / 1024)
+
+    return key_info, net_in, net_out
+
+
+while 1:
+    try:
+         key_info, net_in, net_out = get_rate(get_key)
+
+         for key in key_info:
+             print('%s\nInput:\t %-5sKB/s\nOutput:\t %-5sKB/s\n' % (key, net_in.get(key), net_out.get(key)))
+    except KeyboardInterrupt:
+        exit()
